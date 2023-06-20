c17bd08091dace8f0ea997d673655c3346893530
Ivan Prosyanik <vanyaprosyanik89@gmail.com>
2023-06-20 19:17:33 +0300

    add pc choice

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@ const btnPaper = document.getElementById('paper-btn');
 const btnScissors = document.getElementById('scissors-btn');
 
 const resultBox = document.querySelector('.result__box');
-const enemyBox = document.querySelector('enemy__box')
+const enemyBox = document.querySelector('.result__enemy')
 
 function playGame(playerChoise) {
   const choise = ['rock', 'paper', 'scissors'];
@@ -24,6 +24,13 @@ function playGame(playerChoise) {
     console.log(`You lose :( Computer choised ${computerChoice}`);
     clearRes(lose)
   }
+  if (computerChoice === 'rock') {
+    clearEnem(pcRock)
+  } else if (computerChoice === 'scissors') {
+    clearEnem(pcScissors)
+  } else {
+    clearEnem(pcPaper)
+  }
 }
 
 function clearRes(res) {
@@ -36,26 +43,46 @@ function clearRes(res) {
     resultBox.scrollIntoView({ behavior: 'smooth' });
   }, 1000)
 }
+function clearEnem(res) {
+  enemyBox.innerHTML = ''
+  enemyBox.classList.remove('active');
+  enemyBox.scrollTo({ top: 0, behavior: 'auto' });
+  setTimeout(() => {
+    enemyBox.insertAdjacentHTML('afterbegin', res)
+    enemyBox.classList.add('active')
+    enemyBox.scrollIntoView({ behavior: 'smooth' });
+  }, 1000)
+}
 
 const win = `
 <div class="result__head" style="background-image: url('img/cup.svg');"></div>
-<p class="result__text"><span>Вы выиграли!</span> Поздравляю!</p>
+<p class="result__text"><span>Вы выиграли!</span></p>
 `;
 
 const draw = `
 <div class="result__head" style="background-image: url('img/draw.svg');"></div>
-<p class="result__text"><span>Ничья!</span> Победила дружба!</p>
+<p class="result__text"><span>Ничья!</span></p>
 `;
 
 const lose = `
 <div class="result__head" style="background-image: url('img/lose.svg');"></div>
-<p class="result__text">К сожалению, <span>Вы проиграли.</span> <br> Ну ничего, повезет в другой раз </p>
+<p class="result__text"><span>Вы проиграли.</span></p>
 `;
 
 const pcRock = `
 <article class="header__card card" style="background-image: url('img/rock.svg');"></article>
 <span class="card__name">Камень</span>
-`
+`;
+
+const pcScissors = `
+<article class="header__card card" style="background-image: url('img/scissors.svg');"></article>
+<span class="card__name">Ножницы</span>
+`;
+
+const pcPaper = `
+<article class="header__card card" style="background-image: url('img/paper.svg');"></article>
+<span class="card__name">Бумага</span>
+`;
 
 btnRock.addEventListener('click', () => {
   let playerChoise = 'rock';
